0d9072cbc45a809b5c8dd83bf2b3df5a5aa362f0
Anne-Gert Bultena <agbgit@anne-gert.nl>
2025-06-05 11:10:16 +0200

    Replace tabs by spaces in example-links.js

--- example-links.js
+++ example-links.js
@@ -3,32 +3,34 @@
 var Links = `
 !title Example Favorites
 General
-	Search
-		DuckDuckGo https://duckduckgo.com
-		Google Maps icon=alt https://maps.google.com
-		Wikipedia https://www.wikipedia.org
-		YouTube https://www.youtube.com
-		Google Translate https://translate.google.com/?sl=auto&tl=en&op=translate
-		ChatGPT https://chatgpt.com
-		Wayback Machine https://web.archive.org/
-	Source
-		GitHub favorite-links https://github.com/anne-gert/favorite-links
-		& Direct icon=none https://html-preview.gihub.io/?url=https://github.com/anne-gert/favorite-links/master/favorites.html
+    Search
+        DuckDuckGo https://duckduckgo.com
+        Google Maps icon=alt https://maps.google.com
+        Wikipedia https://www.wikipedia.org
+        YouTube https://www.youtube.com
+        Google Translate https://translate.google.com/?sl=auto&tl=en&op=translate
+        ChatGPT https://chatgpt.com
+        Wayback Machine https://web.archive.org/
+    Source
+        GitHub favorite-links https://github.com/anne-gert/favorite-links
+        & Direct icon=none https://html-preview.gihub.io/?url=https://github.com/anne-gert/favorite-links/master/favorites.html
 Other
-	News
-		nu.nl https://nu.nl
-	Socials
-		Facebook https://www.facebook.com
-			Instagram https://www.instagram.com/
-			WhatsApp Web https://web.whatsapp.com/
-		---
-		LinkedIn https://www.linkedin.com/
-	Firefox
-		Settings icon=firefox about:preferences target=manual
-			All Configuration icon=firefox about:config target=manual
-		All about:* icon=firefox about:about target=manual
+    News
+        nu.nl https://nu.nl
+    Socials
+        Facebook https://www.facebook.com
+            Instagram https://www.instagram.com/
+            WhatsApp Web https://web.whatsapp.com/
+        ---
+        LinkedIn https://www.linkedin.com/
+    Firefox
+        Settings icon=firefox about:preferences target=manual
+            All Configuration icon=firefox about:config target=manual
+        All about:* icon=firefox about:about target=manual
 
-// Icon definitions {{{
+// Icon definitions
 !icondef firefox    https://support.mozilla.org
 `;
+
+// vim: foldmethod=marker nowrap tabstop=4 expandtab
 
